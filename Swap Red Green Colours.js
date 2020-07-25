3a44fee0a2918988bf85c2475ad1b50a58584ef5
Allana Bailey <47748433+allanabailey@users.noreply.github.com>
2020-07-25 20:19:20 +0100

    update swapredgreen with comments and DLTP info

--- Swap Red Green Colours.js
+++ Swap Red Green Colours.js
@@ -1,3 +1,10 @@
+//image taken from the DukeLearnToProgram (DLTP) site - file can be replaced with any.
+//https://www.dukelearntoprogram.com/course1/example/index.php
+
+//new SimpleImage is a method imported from the Edu Duke library
+//Documentation found: https://www.dukelearntoprogram.com/course1/doc/
+
+//Simple method to swap pixels coloured red to green and vice versa.
 var img = new SimpleImage("smallhands.png");
 print(img);
 
