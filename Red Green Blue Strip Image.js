ca48f81c09f1960fadf0ca6eed7846ff1282e949
Allana Bailey <47748433+allanabailey@users.noreply.github.com>
2020-07-25 20:18:37 +0100

    update redgreenblue with comments and DLTP info

--- Red Green Blue Strip Image.js
+++ Red Green Blue Strip Image.js
@@ -1,3 +1,10 @@
+//image taken from the DukeLearnToProgram (DLTP) site - file can be replaced with any.
+//https://www.dukelearntoprogram.com/course1/example/index.php
+
+//new SimpleImage is a method imported from the Edu Duke library
+//Documentation found: https://www.dukelearntoprogram.com/course1/doc/
+
+//Simple method to overlay an image with equal red, green and blue stripes.
 var img = new SimpleImage("lion.jpg");
 for(var pixel of img.values()) {
     if(pixel.getX() < img.getWidth() / 3) {
