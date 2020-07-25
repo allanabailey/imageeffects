d6625614fb4f045f984245ca23b532e1f299be77
Allana Bailey <47748433+allanabailey@users.noreply.github.com>
2020-07-25 20:10:27 +0100

    Updated to include descriptions and sources.

--- Change Blue Duke to Green Duke.js
+++ Change Blue Duke to Green Duke.js
@@ -1,3 +1,10 @@
+//image taken from the DukeLearnToProgram (DLTP) site - file can be replaced with any.
+//https://www.dukelearntoprogram.com/course1/example/index.php
+
+//new SimpleImage is a method imported from the Edu Duke library
+//Documentation found: https://www.dukelearntoprogram.com/course1/doc/
+
+//Simple method to change a completely blue image to green.
 var image = new SimpleImage("duke_blue_devil.png");
 for(var pixel of image.values()) {
 	//if it is blue, change it to green (change fg, not bg)
